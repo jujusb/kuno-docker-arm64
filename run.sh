d57aa40833a8cf043d2026c35512fddc0676e1af
jujusb <jsantilariob@gmail.com>
2026-06-30 07:26:37 +0000

    Refactor environment configuration and remove unused Caddy setup files

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ if [[ $1 == "build" ]]; then
     pushd kuno
     docker buildx build --platform linux/arm64 -t ictrun/kuno:arm64 --load .
     popd
+    exit 0
 fi
 
 source .env
@@ -16,26 +17,9 @@ ensure_network() {
         docker network create "$net"
     fi
 }
-ensure_network caddy_net
+
 ensure_network kuno_internal_net
 
-is_ip() {
-  [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
-}
-
-if [ "$MULTIPLE_CADDY_GLOBAL" = "true" ]; then
-    echo "Multiple global services enabled → using global Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml"
-else
-    echo "Single global service → using local Caddyfile"
-    export COMPOSE_FILE="docker-compose.yml:docker-compose.root-caddy.yml"
-    if is_ip "$KUNO_SERVER_ADDRESS"; then
-        echo "Detected IP → using internal TLS"
-        export TLS_CONFIG='tls internal'
-    else
-        echo "Detected domain → using Cloudflare TLS"
-        export TLS_CONFIG='tls { dns cloudflare {env.CF_API_TOKEN} }'
-    fi
-fi
+export COMPOSE_FILE="docker-compose.yml"
 docker compose $@
 unset COMPOSE_FILE
